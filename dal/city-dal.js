2a82143d89023f3ea86088d73c00ad2530fb1b5c
brookelynn upshall <124837049+Linthorne80@users.noreply.github.com>
2023-12-06 12:13:03 -0330

    Add files via upload

--- dal/city-dal.js
+++ dal/city-dal.js
@@ -1,16 +1,22 @@
-const pool = require('../index');
-
+const { Pool } = require('pg');
+const pool = new Pool({
+  user: 'brook',
+  host: 'localhost',
+  database: 'qap3',
+  password: 'brooksQAP3',
+  port: 5432,
+});
 const getAllCities = async () => {
-  const result = await pool.query('SELECT * FROM city');
+  const result = await pool.query('SELECT name, population FROM public."city"');
   return result.rows;
 };
 
 const createCity = async (name, population) => {
-  await pool.query('INSERT INTO city (name, population) VALUES ($1, $2)', [name, population]);
+  await pool.query('INSERT INTO public.city (name, population) VALUES ($1, $2)', [name, population]);
 };
 
 const updateCity = async (id, name, population) => {
-  await pool.query('UPDATE city SET name = $1, population = $2 WHERE id = $3', [name, population, id]);
+  await pool.query('UPDATE public.city SET name = $1, population = $2 WHERE id = $3', [name, population, id]);
 };
 
 const deleteCity = async (id) => {
